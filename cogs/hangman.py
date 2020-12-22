63b10fc89bde08d3c798fc17e42faa8c8519d615
ChrisLolz <chrisplacey@hotmail.ca>
2020-12-21 21:10:39 -0500

    fixed a bunch of bugs and reorganized code

--- cogs/hangman.py
+++ cogs/hangman.py
@@ -1,31 +1,31 @@
 import discord
-from main import gamehang
 from discord.ext import commands
 
 hangwords = [
     "BLACKLIVESMATTER", "DRAGON", "SCHOOL", "DONALDTRUMP", "JOEBIDEN"
 ]
-print(gamehang)
+
 class hangman(commands.Cog):
     def __init__(self,client):
         self.client = client
-    @commands.Cog.listener()
-    async def on_message(self,message):
-        if message == "!hang" and gamehang == False:
-            gamehang = True
-            channel = client.get_channel(789694075787935778)
-            await channel.send(f"I'm thinking of a {len(hangwords[1])} letter word")
-            game = discord.Embed(title="Hangman")
-            game.set_image(url="https://i.imgur.com/hRzVg1N.png")
-            await channel.send(embed=game)
-        if message.content == hangwords[1]:
-            channel = client.get_channel(789694075787935778)
-            await channel.send("YOU GOT IT POGGERS :open_mouth:")
-            gamehang = False
-        if message.content == "!quit":
-            gamehang = False
-            channel = client.get_channel(789694075787935778)
-            await channel.send("adios amigo")
+        self.lines = "?"*len(hangwords[1])
+
+    @commands.command()
+    async def hang(self,ctx):
+        await ctx.send(f"I'm thinking of a {len(hangwords[1])} letter word")
+        game = discord.Embed(title="Hangman", description=self.lines)
+        game.set_image(url="https://i.imgur.com/hRzVg1N.png")
+        game.set_footer(text="!guess <letter> to guess and !quit to stop playing")
+        await ctx.send(embed=game)
+
+    @commands.command()
+    async def guess(self,ctx,arg):
+        if arg == hangwords[1]:
+            await ctx.send("POGGERS")
+
+    @commands.command()
+    async def quit(self,ctx):
+        await ctx.send("adios amigo")
 
 def setup(client):
     client.add_cog(hangman(client))
--- main.py
+++ main.py
@@ -1,48 +1,33 @@
 import discord
 import os
 from discord.ext import commands
+
 key = os.environ['token']
 intents = discord.Intents.default()
 intents.typing = True
 intents.presences = True
-client = discord.Client(intents=intents)
+client = commands.Bot(command_prefix='!')
+client.remove_command('help')
 
-#cogs
-"""
-@client.command()
-async def load(ctx,extension):
-    client.load_extension(f'cogs.{extension}')
+# variables
 
-@client.command()
-async def unload(ctx,extension):
-    client.unload_extension(f'cogs.{extension}')
-
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        client.load_extension(f'cigs,{filename[:-3]}')
-"""
-#variables
-global gamehang
-gamehang = False
-
+# look guys
 @client.event
 async def on_member_update(before, after):
+    print('hi')
     if str(before.status) == "online":
         if str(after.status) == "offline":
             channel = client.get_channel(789694075787935778)
             await channel.send(f"LOOK GUYS HE DISCONNECTED {after.mention}")
 
-@client.event
-async def on_message(message):
-    if gamehang == False:
-        if message.content == "!help":
-            channel = client.get_channel(789694075787935778)
-            await channel.send("Type !hang to play hangman :flushed:\nType !clear to clear all messages in the channel")
-        if message.content == "!hang":
-            gamehang == True
-        if message.content == "!clear":
-            channel = client.get_channel(789694075787935778)
-            await channel.purge()
+# commands
+@client.command()
+async def help(ctx):
+    await ctx.send("Type !hang to play hangman :flushed:\nType !clear to clear all messages in the channel")
 
-client.load_extension('cogs.hangman')
+@client.command()
+async def clear(ctx):
+    await ctx.channel.purge()
+
+client.load_extension("cogs.hangman")
 client.run(key)
